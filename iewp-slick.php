67a0a5a9ea4ea9a103438c6e1e4f7abb6656b04d
Philip Newborough <corenominal@corenominal.org>
2016-09-11 21:00:38 +0100

    Main plugin file

---
+++ iewp-slick.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined( 'WPINC' ) ) { die('Direct access prohibited!'); }
+/**
+ * Plugin Name: IEWP Slick
+ * Plugin URI: https://github.com/corenominal/iewp-slick
+ * Description: A WordPress Plugin for Slick - the last carousel you'll ever need
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
