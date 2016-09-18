5ce2174851610291b3ee0721eda1594b65b62e26
Philip Newborough <corenominal@corenominal.org>
2016-09-18 17:45:12 +0100

    Prep enqueue scripts

--- admin/iewp_slick_carousels_edit.php
+++ admin/iewp_slick_carousels_edit.php
@@ -2,6 +2,24 @@
 if ( ! defined( 'WPINC' ) ) { die('Direct access prohibited!'); }
 
 /**
+ * Enqueue additional JavaScript and CSS
+ */
+function iewp_iewp_slick_carousels_edit_scripts( $hook )
+{
+
+	if( 'slick-carousels_page_iewp_slick_carousels_edit' != $hook )
+	{
+		return;
+	}
+
+	wp_register_script( 'iewp_slick_carousels_edit_js', plugin_dir_url( __FILE__ ) . 'js/iewp_slick_carousels_edit.js', array('jquery'), '0.0.1', true );
+	wp_enqueue_script( 'iewp_slick_carousels_edit_js' );
+
+	wp_enqueue_media();
+}
+add_action( 'admin_enqueue_scripts', 'iewp_iewp_slick_carousels_edit_scripts' );
+
+/**
  * Output HTML
  */
 function iewp_slick_carousels_edit_callback()
