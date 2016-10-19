f8d2a13a6b2072decbb54df3f1b0dea3114911ed
Philip Newborough <corenominal@corenominal.org>
2016-10-19 15:57:40 +0100

    Method for removing carousels

--- admin/js/iewp_slick_carousels.js
+++ admin/js/iewp_slick_carousels.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($)
         var endpoint = $( '#iewp-slick-carousels' ).data( 'endpoint' );
         var data = {
             apikey: $( '#iewp-slick-carousels' ).data( 'apikey' ),
-            action: 'get_carousels'
+            action: 'list_carousels'
         };
         $.ajax({
             url: endpoint,
@@ -15,8 +15,29 @@ jQuery(document).ready(function($)
             data: data
         })
         .done(function( data ) {
-            console.log( data );
-            console.log( 'foo' );
+            var carousels = '';
+			if( data.num_rows == 0 )
+			{
+				carousels = '<tr><td colspan="3">No carousels found!</td></tr>';
+			}
+			else
+			{
+				$.each(data.carousels, function(i, carousel)
+				{
+					carousels += '<tr><td>' + carousel.name + '</td>';
+	        		carousels += '<td>' + carousel.images + '</td>';
+					carousels += '<td class="iewp-slick-carousel-options-cell' + carousel.id + '">';
+                    carousels += '<button data-id="' + carousel.id + '" class="button iewp-slick-edit-carousel-button">Edit</button> ';
+					carousels += '<button data-id="' + carousel.id + '" class="button iewp-slick-remove-carousel-button">Remove</button>';
+					carousels += '<div class="remove-carousel-prompt remove-carousel-prompt' + carousel.id + '">';
+					carousels += '<span>Are you sure?</span>';
+					carousels += '<button data-id="' + carousel.id + '" class="button remove-carousel-prompt-yes">Yes</button> ';
+					carousels += '<button data-id="' + carousel.id + '" class="button remove-carousel-prompt-no">No</button>';
+					carousels += '</div>';
+					carousels += '</td></tr>';
+				});
+			}
+			$( '#the-list' ).html( carousels );
         })
         .fail(function( data ) {
             console.log( "error" );
@@ -24,5 +45,51 @@ jQuery(document).ready(function($)
     }
 
     get_carousels();
+
+    /**
+	 * Remove carousel from list
+	 */
+	$( document ).on( 'click', '.iewp-slick-remove-carousel-button', function( e )
+	{
+		e.preventDefault();
+		var id = $( this ).attr( 'data-id' );
+		$( '.remove-carousel-prompt' ).not( '.remove-carousel-prompt' + id ).slideUp();
+		$( '.remove-carousel-prompt' + id ).slideToggle();
+	});
+
+    $( document ).on( 'click', '.remove-carousel-prompt-no', function( e )
+	{
+		e.preventDefault();
+		$( '.remove-carousel-prompt' ).slideUp();
+	});
+
+    $( document ).on( 'click', '.remove-carousel-prompt-yes', function( e )
+	{
+		e.preventDefault();
+		var endpoint = $( '#iewp-slick-carousels' ).data( 'endpoint' );
+		var data = {
+			action: 'delete_carousel',
+			id: $( this ).attr( 'data-id' ),
+            apikey: $( '#iewp-slick-carousels' ).data( 'apikey' )
+        };
+		$.ajax(
+		{
+			url: endpoint,
+			type: 'GET',
+			dataType: 'json',
+			data: data
+		})
+		.done(function( data )
+		{
+            console.log( data );
+            var carousels = '<tr><td colspan="3">Refreshing plugins ...</td></tr>';
+			$( '#the-list' ).html( carousels );
+			get_carousels();
+		})
+		.fail(function()
+		{
+			console.log("OH NOES! AJAX error");
+		});
+	});
 
 });
--- endpoints/carousels_admin.php
+++ endpoints/carousels_admin.php
@@ -8,6 +8,50 @@ function iewp_slick_carousels_admin( $request_data )
 
     $data = $request_data->get_params();
 
-    return $data;
+    /**
+	 * Test for action
+	 */
+	if( !isset( $data['action'] ) )
+ 	{
+ 		$data['error'] = 'Please provide an action';
+ 		return $data;
+ 	}
+
+    switch ( $data['action'] )
+	{
+		case 'list_carousels':
+			global $wpdb;
+			$sql = "SELECT iewp_slick_carousels.id, name, COUNT(carousel_id) AS images
+                      FROM iewp_slick_carousels LEFT JOIN iewp_slick_carousel_images
+                        ON iewp_slick_carousels.id = iewp_slick_carousel_images.carousel_id
+                     GROUP BY iewp_slick_carousels.id
+                     ORDER BY iewp_slick_carousels.id DESC;";
+			$data['carousels'] = $wpdb->get_results( $sql, ARRAY_A );
+			$data['num_rows'] = $wpdb->num_rows;
+			unset( $data['action'] );
+			unset( $data['apikey'] );
+			return $data;
+			break;
+
+        case 'delete_carousel':
+            if( $data['apikey'] != $apikey )
+            {
+                $data['error'] = 'Invalid API key';
+                return $data;
+            }
+            global $wpdb;
+			$wpdb->delete( 'iewp_slick_carousels', array( 'id' => $data['id'] ), array( '%d' ) );
+            $wpdb->delete( 'iewp_slick_carousel_images', array( 'carousel_id' => $data['id'] ), array( '%d' ) );
+
+			unset( $data['action'] );
+			unset( $data['apikey'] );
+			return $data;
+			break;
+
+		default:
+			$data['error'] = 'Please provide an action';
+			return $data;
+			break;
+	}
 
 }
